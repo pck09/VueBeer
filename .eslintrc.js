a0b9d9667833cd8ae64d265a40879c5f08929912
pck <balint.polczman@gmail.com>
2019-02-21 08:01:27 +0100

    init config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": [
+      "standard",
+      "plugin:vue/recommended"
+    ],
+    "plugins": [
+      "import",
+      "node",
+      "standard",
+      "vue"
+    ],
+    "rules": {
+      "vue/max-attributes-per-line": 0,
+      "vue/component-name-in-template-casing": ["error", "kebab-case"]
+    }
+  }
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /(\.scss)$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: function () {
+                return [
+                    require('autoprefixer')
+                ]
+              }
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+                sourceMap: true
+            }
+          }]
+        })
+    }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
